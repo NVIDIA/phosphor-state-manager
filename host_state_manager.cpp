94c7acf67693d64399a4e8c8867d5315ca6549f0
leonhuang <leonhuang@nvidia.com>
2025-01-22 23:33:32 -0800

    [GB200NVL] System will record “Unknown” SEL after do power reset
    Root Cause:
    Because there is no corresponding condition,
    and BMC directly assigns transitionStr to unknown.
    
    Change List:
    1. Add ForceWarmReboot in requestedHostTransition function.
    
    Fixes nvbug https://nvbugspro.nvidia.com/bug/5052323
    
    Signed-off-by: leonhuang@nvidia.com

--- host_state_manager.cpp
+++ host_state_manager.cpp
@@ -448,6 +448,12 @@ Host::Transition Host::requestedHostTransition(Transition value)
     {
         transitionStr = "GracefulWarmReboot";
     }
+#if ENABLE_FORCE_WARM_REBOOT
+    else if (value == Transition::ForceWarmReboot)
+    {
+        transitionStr = "ForceWarmReboot";
+    }
+#endif
     else
     {
         transitionStr = "Unknown";
